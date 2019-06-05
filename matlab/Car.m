5fd8e874674146fb22496aabf99748472d0867ee
unknown <maciek2608@gmail.com>
2019-06-05 23:13:36 +0200

    Sending steering to the car from MATLAB

--- matlab/Car.m
+++ matlab/Car.m
@@ -8,6 +8,15 @@ classdef Car < handle
 		position = zeros(1, 2);
 		name;
 		updateMode;
+		
+		steeringLeftHandle  = -1;
+		steeringRightHandle = -1;
+		
+		motorLeftHandle  = -1;
+		motorRightHandle = -1;
+		
+		d = 0.755;  % 2*d=distance between left and right wheels
+		l = 2.5772; % l=distance between front and read wheels
 	end
 	
 	methods
@@ -17,16 +26,31 @@ classdef Car < handle
 			obj.handle     = handle;
 			obj.updateMode = vrepComm.vrep.simx_opmode_streaming;
 			
-			[childHandles, childNames] = vrepComm.getChildHandles(handle);
-			proximitySensorHandle = childHandles(contains(childNames, "proximitySensors", 'IgnoreCase', true));
+			[carChildHandles, childNames] = vrepComm.getChildHandles(handle);
+			proximitySensorHandle = carChildHandles(contains(childNames, "proximitySensors", 'IgnoreCase', true));
 			[laserPointerHandles, obj.laserNames] = vrepComm.getChildHandles(proximitySensorHandle);
-
+			
+			leftDamperHandle = carChildHandles(contains(childNames, "frontLeftSpring", 'IgnoreCase', true));
+			[leftCylinder, ~] = vrepComm.getChildHandles(leftDamperHandle);
+			[obj.steeringLeftHandle, ~] = vrepComm.getChildHandles(leftCylinder);
+			[leftCylinder, ~] = vrepComm.getChildHandles(obj.steeringLeftHandle);
+			[obj.motorLeftHandle, ~] = vrepComm.getChildHandles(leftCylinder);
+			
+			rightDamperHandle = carChildHandles(contains(childNames, "frontRightSpring", 'IgnoreCase', true));
+			[rightCylinder, ~] = vrepComm.getChildHandles(rightDamperHandle);
+			[obj.steeringRightHandle, ~] = vrepComm.getChildHandles(rightCylinder);
+			[rightCylinder, ~] = vrepComm.getChildHandles(obj.steeringRightHandle);
+			[obj.motorRightHandle, ~] = vrepComm.getChildHandles(rightCylinder);
+			
 			i = 1;
 			for laserPointerHandle = laserPointerHandles
 				[childHandles, childNames] = vrepComm.getChildHandles(laserPointerHandle);
 				obj.laserHandles(i) = childHandles(contains(childNames, "sensor", 'IgnoreCase', true));
 				i = i+1;
 			end
+			
+			
+			
 			obj.updateLasers();
 			obj.updatePosition();
 			obj.updateMode = vrepComm.vrep.simx_opmode_buffer;
@@ -48,6 +72,28 @@ classdef Car < handle
 		function update(obj)
 			obj.updateLasers();
 			obj.updatePosition();
+		end
+		
+		function setSpeed(obj, desiredSpeed)
+			obj.vrepComm.vrep.simxSetJointTargetVelocity(obj.vrepComm.clientID, obj.motorLeftHandle,  desiredSpeed, obj.vrepComm.vrep.simx_opmode_streaming);
+			obj.vrepComm.vrep.simxSetJointTargetVelocity(obj.vrepComm.clientID, obj.motorRightHandle, desiredSpeed, obj.vrepComm.vrep.simx_opmode_streaming);
+		end
+		
+		function setAngle(obj, desiredAngle)
+			if (desiredAngle > 45)
+				desiredAngle = 45;
+			end
+			if (desiredAngle < -45)
+				desiredAngle = -45;
+			end
+			
+			desiredAngle = desiredAngle*pi/180;
+			
+			steeringAngleLeft  = atan(obj.l/(-obj.d+obj.l/tan(desiredAngle)));
+			steeringAngleRight = atan(obj.l/(obj.d+obj.l/tan(desiredAngle)));
+		
+			obj.vrepComm.vrep.simxSetJointTargetPosition(obj.vrepComm.clientID, obj.steeringLeftHandle,  steeringAngleLeft,  obj.vrepComm.vrep.simx_opmode_streaming);
+			obj.vrepComm.vrep.simxSetJointTargetPosition(obj.vrepComm.clientID, obj.steeringRightHandle, steeringAngleRight, obj.vrepComm.vrep.simx_opmode_streaming);
 		end
 	end
 end
